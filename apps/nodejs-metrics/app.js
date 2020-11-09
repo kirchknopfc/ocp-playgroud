d717f1d655adad4965f730ce5eb699c0a06464a4
Kirchknopf, Christian <christian.kirchknopf@atos.net>
2020-11-09 12:46:34 +0100

    env variabels, logging

--- apps/nodejs-metrics/app.js
+++ apps/nodejs-metrics/app.js
@@ -1,12 +1,20 @@
 'use strict';
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 
-// Constants
+// Env Variabels
 const port = process.env.PORT || "8080";
+const metricsFile = process.env.METRICS_FILE || 'metrics/prometheus.txt';
+const metricsUrl = process.env.METRICS_URL || '/actuator/prometheus';
 
 // APP
 const app = express();
+
+// Logging
+app.use(morgan('combined'))
+
+// Root
 app.get('/', function (req, res) {
   res.send('<h1>NodeJs Metrics Sample App</h1><br><a href="./actuator/prometheus">metrics<a/><br><a href="./env">env<a/>');
 });
@@ -17,8 +25,8 @@ app.get('/env', function (req, res) {
   res.send(process.env);
 });
 
-// prometheus metrics
-app.use('/actuator/prometheus', express.static(path.join(__dirname, 'metrics/prometheus.txt')));
+// metrics
+app.use(metricsUrl, express.static(path.join(__dirname, metricsFile)));
 
 // Start Server
 app.listen(port, () => {
